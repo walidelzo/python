10fc31c08ab196ed07af86ad61afb4622c088e09
walidelzo <walidezo@gmail.com>
2021-12-22 13:38:32 +0200

    Initial commit

---
+++ indentation.py
@@ -0,0 +1,26 @@
+if 5 > 4 :
+    print("the number is greater than")
+
+#this is comment
+x = 5 
+y = 6
+if x > y:
+    print("z > y")
+else:
+    print("y > x")
+
+
+"""
+this is multiline comment
+yes it is
+you can see that 
+"""
+
+x= "hello world"
+print(x)
+
+##the calculator programm 
+num1= 123
+num2=123
+sum = num1+num2
+print(sum)
---
+++ varaibles.py
@@ -0,0 +1,18 @@
+#create variables 
+
+x = 12
+y = "yahoo.com"
+firstName = "walid"
+secondname = "mahmoud"
+
+fullName = firstName + " "  + secondname
+print(fullName)
+
+print(type(x))
+print(type(y))
+
+# casting variables
+number1 = 1
+number1 = str(number1)
+print("---------------")
+print(type(number1))
